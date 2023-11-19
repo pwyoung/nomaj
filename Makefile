18a61914ff34fd41522f9815125cef1d13685266
phillip young <phil.w.young@gmail.com>
2023-11-19 00:18:17 -0500

    wip: automatic. TODO: rebase this

--- Makefile
+++ Makefile
@@ -8,6 +8,10 @@ TESTS=$(wildcard ./tests*/unit-tests/*)
 
 SSHCFG=$$HOME/.ssh/config
 
+default: test
+
+FORCE:
+
 test: deps
 	$(info tests)
 	echo "Run all tests: $(TESTS)"
@@ -19,17 +23,21 @@ test: deps
 		make -C $$test clean; \
 	done
 
-python3:
+python3: FORCE
 	$(info Install pips)
 	pip3 -q install -r requirements.txt
 
-ssh-config:
+ssh-config: FORCE
 	$(info SSH config file must exist)
 	test -f $(SSHCFG) || echo "creating $(SSHCFG)" && touch $(SSHCFG)
 
-passwordless-ssh:
+passwordless-ssh: FORCE
 	$(info Passwordless SSH must work)
 	ssh localhost whoami || echo 'WARNING: passwordless SSH failed. This is used by complex Ansible playbooks such as the K8S installer.'
+
+os-packages: FORCE
+	$(info Install os-package dependencies)
+	./deps/nomaj-deps.sh
 
 deps: python3 ssh-config passwordless-ssh
 	$(info deps)
---
+++ deps/nomaj-deps.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# GOAL:
+#   Install deps for nomaj
+#
+# Requirements
+# - This was tested on Ubuntu-22.04
+
+set -e
+
+PKGS="emacs-nox tree glances htop dmidecode"
+PKGS+=" gnome-boxes virt-manager"
+PKGS+=" python3-pip vagrant "
+#PKGS+=" xdg-utils"
+#PKGS+=" xfsprogs"
+PKGS+=" python-is-python3 python3-venv"
+# For local certs, esp on K8S
+# https://github.com/cloudflare/cfssl
+#PKGS+=" golang-cfssl"
+
+install_packages() {
+    sudo apt update
+    sudo apt install -y $PKGS
+}
+
+# Support this user running "user sessions" with KVM/Qemu
+# See: https://mike42.me/blog/2019-08-how-to-use-the-qemu-bridge-helper-on-debian-10
+# See: https://www.linuxtechi.com/how-to-install-kvm-on-ubuntu-22-04/
+configure_qemu_helper() {
+    # This didn't exist
+    sudo mkdir -p /etc/qemu
+    sudo chown root /etc/qemu
+    sudo chmod 0755 /etc/qemu
+
+    echo 'allow virbr0' | sudo tee /etc/qemu/bridge.conf
+    sudo chown root.$USER /etc/qemu/bridge.conf
+    sudo chmod 0664 /etc/qemu/bridge.conf
+    #
+    sudo chmod u+s /usr/lib/qemu/qemu-bridge-helper
+    #
+    sudo chgrp $USER /etc/qemu/bridge.conf
+
+    if ! systemctl status libvirtd.service | grep 'active (running)'; then
+        cat <<EOF
+        # Setup libvirt daemon
+        systemctl enable libvirtd.service
+        systemctl start libvirtd.service
+EOF
+        exit 1
+    fi
+
+    if ! ip addr show virbr0 | grep '192.168.122.1'; then
+        cat <<EOF
+        # Create "virbr0" as the default bridge for Libvirt/KVM/Qemu
+        sudo virsh net-autostart --network default
+        sudo virsh net-start --network default
+EOF
+        exit 1
+    fi
+
+
+    # instead of "-c qemu:///session"
+    export LIBVIRT_DEFAULT_URI="qemu:///session"
+    virsh list --all
+
+    # ls -l /var/run/libvirt/libvirt-sock
+    # srwxrwx--- 1 root libvirt 0 Dec  7 20:40 /var/run/libvirt/libvirt-sock
+    #
+    # sudo usermod -G libvirt-qemu -a $USER
+    # sudo usermod -G libvirt -a $USER
+    # sudo usermod -G kvm -a $USER
+    # reboot
+
+    #
+    # Setup libvirtd to run as non-root
+    #   https://computingforgeeks.com/use-virt-manager-as-non-root-user/
+    #
+    # sudo emacs /etc/libvirt/libvirtd.conf
+    # unix_sock_group = "libvirt"
+    # unix_sock_rw_perms = "0770"
+    #
+    # sudo systemctl restart libvirtd.service
+    # systemctl status libvirtd.service
+
+    # e ~/.profile.d/kvm.sh
+
+
+}
+
+setup_ansible() {
+    if ! ansible-galaxy --version; then
+        sudo apt install software-properties-common -y
+        sudo apt-add-repository ppa:ansible/ansible
+        sudo apt update -y
+        sudo apt install ansible -y
+        ansible --version
+        ansible-galaxy --version
+    fi
+}
+
+setup_ssh() {
+    # SSH
+    if [ ! -e ~/.ssh/id_ed25519 ]; then
+        echo "Make ed25519 SSH key"
+        ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519
+    fi
+
+    # SSH
+    sudo apt-get install -y openssh-server
+    sudo systemctl enable ssh --now
+
+    # Passwordless-SSH should work
+    ssh localhost hostname
+}
+
+
+
+main() {
+    install_packages
+    configure_qemu_helper
+    setup_ansible
+    setup_ssh   
+}
+
+main
